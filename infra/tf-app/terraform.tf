a1e2eea81bb0d2169f31c466f62dbdce42a5a751
Abhinav Abhinav <abhinaavv007@gmail.com>
2025-03-29 00:32:39 -0400

    Updated

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -2,29 +2,20 @@
 
 
 terraform {
-    
-  required_version = "~> 1.5"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.96.0"
-    }
-  }
-
   backend "azurerm" {
     resource_group_name  = "abhi0012-githubactions-rg"
     storage_account_name = "abhi0012githubactions"
     container_name       = "tfstate"
     key                  = "prod.app.tfstate"
     use_oidc             = true
-    access_key           = var.ARM_ACCESS_KEY # Reference ARM access key dynamically
+    access_key           = "clHg+NlOqRxk+mrMhhqLJ5pLnVzG3/x8IWLinzue1cAHvI8kgjkgSWzSXp6EKzmfoN0XeCAnsbYG+AStY5Iu0g==" # Reference ARM access key dynamically
   }
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "454cef6e-c0dd-444f-9766-2fcc4e902f67"
-  use_oidc = true 
+  use_oidc        = true
 }
 
 resource "azurerm_resource_group" "app_rg" {
